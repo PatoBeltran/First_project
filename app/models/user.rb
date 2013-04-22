94e51ca95f81fc91e2ed99250ee3dda84dbb1c39
Patricio Beltran <pbelsal@gmail.com>
2013-04-22 17:31:22 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,13 +21,19 @@ class User < ActiveRecord::Base
   					format: { with: VALID_EMAIL_REGEX }, 
   					uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true, 
-  					   length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 
   validates :password_confirmation, presence: true
  
   before_save { email.downcase! }
 
+  before_save :create_remember_token
+
   has_secure_password
 
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end

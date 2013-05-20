aba3a1f681969fdce27b49cebc3759c561f438e8
Patricio Beltran <pbelsal@gmail.com>
2013-05-19 23:14:29 -0500

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ FirstProject::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
-
+  resources :relationships, only: [:create, :destroy]
+  resources :users do
+      member do
+        get :following, :followers
+      end
+    end
 
   root to: 'static_pages#home'
 
